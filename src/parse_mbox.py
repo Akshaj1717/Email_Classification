b667030bbb2424ea5a49a6525832097c17d61b75
Akshaj Sinha <akshaj17@gmail.com>
2026-06-27 14:58:50 -0400

    moving requirements.txt to src folder

--- src/parse_mbox.py
+++ src/parse_mbox.py
@@ -1,1 +1,11 @@
-# extracts emails from .mbox into structured format
+# extracts emails from .mbox into structured format using pandas dataframe
+
+import argparse
+import mailbox
+import re
+from datetime import datetime
+from email.utils import parsedate_to_datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
