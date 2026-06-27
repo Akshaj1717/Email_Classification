a1d2c48a3fd0745dc9581be787450406a5f4c658
Akshaj Sinha <akshaj17@gmail.com>
2026-06-27 15:04:40 -0400

    beginning parse_mbox implementation

--- src/parse_mbox.py
+++ src/parse_mbox.py
@@ -9,3 +9,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+def strip_html(html_text: str) -> str:
+    # removing HTML tags from email body and handling whitespace to return readable text
+    if not html_text:
+        return ""
+    soup = BeautifulSoup(html_text, "html.parser")
+
+    # removing script and style elements 
+    for tag in soup(["script", "style"]):
+        tag.decompose()
+    text = soup.get_text(seprator="")
+    
+    
