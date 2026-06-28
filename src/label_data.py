6b9b85b7e55f92431579049035c0c4ac3dc97e80
Akshaj Sinha <akshaj17@gmail.com>
2026-06-28 00:41:57 -0400

    starting label_data implementation

--- src/label_data.py
+++ src/label_data.py
@@ -1,1 +1,27 @@
-# applies weak labels from gmail categories
+# takes parsed data from parse_mbox and produces labeld data
+# flags candidate job/application emails using sender/subject heuristics
+# outputs CSV with candidates for manual review
+
+import argparse
+import re
+import pandas as pd
+
+# collection of known ATS and job-platforms
+JOB_SENDER_DOMAINS = [
+    "greenhouse.io",
+    "lever.co",
+    "myworkday.com",
+    "workday.com",
+    "icims.com",
+    "smartrecruiters.com",
+    "jobvite.com",
+    "ashbyhq.com",
+    "taleo.net",
+    "successfactors.com",
+    "indeedemail.com",
+    "linkedin.com",  
+    "handshake.com",
+    "ziprecruiter.com",
+    "glassdoor.com",
+]
+
