bfe031b2bf1827a95d16d19ce1d1d9b5c5a52e20
Akshaj Sinha <akshaj17@gmail.com>
2026-06-30 20:37:21 -0400

    exploring the job candidate field

--- src/explore_candidates.py
+++ src/explore_candidates.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import re
 
 dataset = pd.read_csv("data/processed/emails_labeled.csv")
+
+df = pd.DataFrame(dataset, columns=['date','sender','subject','body','gmail_labels','gmail_category','job_candidate'])
+print(df.loc[df['job_candidate'] == True])
+print(df['job_candidate'].value_counts())
+
+sender_domain = pd.Series.str.extract(df['sender'], re.search(r'@([A-Za-z0-9.-]+)'))
