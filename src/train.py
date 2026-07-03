8c5aa4cec8d1f7b9e934dbf6efacf3267c47514b
Akshaj Sinha <akshaj17@gmail.com>
2026-07-03 19:41:26 -0400

    debugging

--- src/train.py
+++ src/train.py
@@ -3,13 +3,17 @@
 import pandas as pd
 import datetime
 
-df = pd.read_csv("data/parsed_emails.csv")
+df = pd.read_csv("data/processed/emails_labeled.csv")
 
 df["date"] = pd.to_datetime(df["date"], utc=True)
+print(df['date'].dtype)
+print(df['date'].min(), df['date'].max())
 
 current_time = pd.Timestamp.now(tz="utc")
 cutoff_time = current_time - pd.DateOffset(years=2)
 
+df = df[df["date"] >= cutoff_time]
+
 df.dropna(subset=["date"], inplace=True)
-print(f"Total Emails:", df.value_counts())
+print(f"Total Emails:", len(df))
 print("Label Breakdown:", df["label"].value_counts())
