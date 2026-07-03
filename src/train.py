7ff2b9b56c9f2d923d63ac3dde15a781cf12692b
Akshaj Sinha <akshaj17@gmail.com>
2026-07-03 19:25:45 -0400

    filtering to past two years and checking the email breakdown

--- src/train.py
+++ src/train.py
@@ -1,5 +1,15 @@
 # train model
 
 import pandas as pd
+import datetime
 
 df = pd.read_csv("data/parsed_emails.csv")
+
+df["date"] = pd.to_datetime(df["date"], utc=True)
+
+current_time = pd.Timestamp.now(tz="utc")
+cutoff_time = current_time - pd.DateOffset(years=2)
+
+df.dropna(subset=["date"], inplace=True)
+print(f"Total Emails:", df.value_counts())
+print("Label Breakdown:", df["label"].value_counts())
