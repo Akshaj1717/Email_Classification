352f1b0f1996ed9f83d4c633533620bdb23f6000
Akshaj Sinha <akshaj17@gmail.com>
2026-07-03 19:44:24 -0400

    dropped irrelavant email categories

--- src/train.py
+++ src/train.py
@@ -6,13 +6,12 @@ import datetime
 df = pd.read_csv("data/processed/emails_labeled.csv")
 
 df["date"] = pd.to_datetime(df["date"], utc=True)
-print(df['date'].dtype)
-print(df['date'].min(), df['date'].max())
 
 current_time = pd.Timestamp.now(tz="utc")
 cutoff_time = current_time - pd.DateOffset(years=2)
 
 df = df[df["date"] >= cutoff_time]
+df = df[df["label"] != "Forums"]
 
 df.dropna(subset=["date"], inplace=True)
 print(f"Total Emails:", len(df))
