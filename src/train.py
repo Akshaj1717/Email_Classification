08558cc5675ab93a4856f585f8b7c6c7c39644b0
Akshaj Sinha <akshaj17@gmail.com>
2026-07-05 16:51:18 -0400

    step 4 which was the vectorization process

--- src/train.py
+++ src/train.py
@@ -7,6 +7,7 @@ import datetime
 import email
 from email.header import decode_header
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv("data/processed/emails_labeled.csv")
  
@@ -42,3 +43,8 @@ print(df["text"].head(3))
 
 # step 3: train/test split 
 X_train, X_test, y_train, y_test = train_test_split(df["text"], df["label"], test_size=0.2, random_state=42, stratify=df["label"])
+
+# step 4: TF-IDF vectorization
+vectorizer = TfidfVectorizer(max_features=50000, ngram_range=(1,2), sublinear_tf=True)
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
